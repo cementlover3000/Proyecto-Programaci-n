13e7644b8b29f62b2b4eacd65c140b77eeac5e88
alebragon <alebragoncac200@gmail.com>
2025-01-06 16:46:49 +0100

    Update TAD_Tablero.h

--- src/TAD_Tablero.h
+++ src/TAD_Tablero.h
@@ -1,1 +1,18 @@
+/*
+ *
+ * FUNDAMENTOS DE PROGRAMACIÓN
+ *  Curso 2024/2025
+
+    Nombre: TAD_Tablero.h
+    Descripción: Especificación del TAD Tablero para el proyecto Number Tiles
+    		       encargado de gestionar la información del tablero.
+
+    Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
+
+ *
+ */
+
+#ifndef NUMBERTILES_H_
+#define NUMBERTILES_H_
+
 
