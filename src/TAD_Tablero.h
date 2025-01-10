9848b4a753c93cbccc6537ab4445b04036a288a9
cementlover3000 <thewhenthe@gmail.com>
2025-01-10 17:38:38 +0100

    Update TAD_Tablero.h

--- src/TAD_Tablero.h
+++ src/TAD_Tablero.h
@@ -25,39 +25,48 @@ struct Tablero{
 
 //PRE:{n tiene que ser múltiplo de 2, 0<=fila<7, 0<=columna<7}
 //POST:{inserta el valor n en la casilla del tablero indicada por fila y columna}
+//Complejidad O(1)
 void insertarValorTablero(Tablero &t,int n, int fila,int columna);
 
 //PRE:{0<=fila<7, 0<=columna<7, la casilla debe de estar llena}
 //POST:{devuelve el valor de la casilla del tablero indicada por fila y columna}
+//Complejidad O(1)
 int obtenerValorTablero(Tablero t,int fila,int columna);
 
 //PRE:{ 0<=columna<7}
 //POST:{devuelve true si la columna está llena y false en caso contrario}
+//Complejidad O(n)
 bool columnaEstaLlena(Tablero t,int columna);
 
 //PRE:{}
 //POST:{devuelve true si el tablero está lleno y false en caso contrario}
+//Complejidad O(n^2)
 bool tableroEstaLleno(Tablero t);
 
 //PRE:{0<=columna<7}
 //POST:{elimina las casillas vacías intermedias y desplaza los elementos de una determinada casilla}
+//Complejidad O(n)
 void eliminarCasillasVaciasIntermedias(Tablero &t, int columna);
 
 //PRE:{0<=fila<7, 0<=columna<7}
 //POST:{devuelve true si la casilla está vacía y false en caso contrario}
+//Complejidad O(1)
 bool casillaEstaVacia(Tablero t, int fila, int columna);
 
 //PRE:{0<=fila<7, 0<=columna<7}
 //POST:{elimina el valor que se encuentra en la casilla determinada por fila y columna y deplaza los 
 //elementos de la columna que se encuentran encima de ella hacia abajo}
+//Complejidad O(n)
 void vaciarCasillaTablero(Tablero &t, int fila, int columna);
 
 //PRE:{0<=columna<7, para que el módulo funcione, la columna no puede estar llena}
 //POST:{devuelve la fila de la primera casilla vacía de la columna dada}
+//Complejidad O(n)
 int filaPrimeraCasillaVacia(Tablero t,int columna);
 
 //PRE:{}
 //POST:{devuelve el número de filas que hay en el tablero}
+////Complejidad O(1)
 int numeroFilas(Tablero t);
 
 
