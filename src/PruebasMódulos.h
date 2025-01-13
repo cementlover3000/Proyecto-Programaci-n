e473fccedf1f5b74f9ac83bc803e89b605a3581f
cementlover3000 <thewhenthe@gmail.com>
2025-01-13 17:05:01 +0100

    Add files via upload

---
+++ src/PruebasMódulos.h
@@ -0,0 +1,36 @@
+/*
+ * PruebasMódulos.h
+ *
+ *  Created on: 13 ene 2025
+ *      Author: Abraham
+ */
+
+#ifndef PRUEBASMÓDULOS_H_
+#define PRUEBASMÓDULOS_H_
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+void testIniciar();
+
+
+void testEstaVacia();
+
+
+void testVaciarCasilla();
+
+
+void testInsertarValor();
+
+
+void testDevolverValor();
+
+
+void pruebasTADCasilla();
+
+
+#endif /* PRUEBASMÓDULOS_H_ */
