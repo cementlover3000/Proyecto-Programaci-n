052f28345fd257e75d12619de43d4c5b39eab09a
cementlover3000 <thewhenthe@gmail.com>
2025-01-13 18:49:30 +0100

    Update TAD_Tablero.h

--- src/TAD_Tablero.h
+++ src/TAD_Tablero.h
@@ -3,29 +3,25 @@
  * FUNDAMENTOS DE PROGRAMACIÓN
  *  Curso 2024/2025
 
-	Nombre: TAD_Tablero.h
-	Descripción: Especificación del TAD Tablero para el proyecto Number Tiles
-   		    	encargado de gestionar la información del tablero.
+    Nombre: TAD_Tablero.h
+    Descripción: Especificación del TAD Tablero para el proyecto Number Tiles
+    		       encargado de gestionar la información del tablero.
 
-	Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
+    Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
 
  *
  */
 
 #ifndef TAD_TABLERO_H_
 #define TAD_TABLERO_H_
-#include <iostream>
-using namespace std;
-
 #include "TAD_Casilla.h"
 
 const int FILAS=7;
 const int COLUMNAS=7;
 typedef Casilla tMatriz[FILAS][COLUMNAS];
-
 struct Tablero{
-	tMatriz tablero;
-	int ocupadas;
+    tMatriz tablero;
+    int ocupadas;
 };
 
 //PRE:{n tiene que ser múltiplo de 2, 0<=fila<7, 0<=columna<7}
@@ -48,20 +44,4 @@ bool tableroEstaLleno(Tablero t);
 //POST:{elimina las casillas vacías intermedias y desplaza los elementos de una determinada casilla}
 void eliminarCasillasVaciasIntermedias(Tablero &t, int columna);
 
-//PRE:{0<=fila<7, 0<=columna<7}
-//POST:{devuelve true si la casilla está vacía y false en caso contrario}
-bool casillaEstaVacia(Tablero t, int fila, int columna);
-
-//PRE:{0<=fila<7, 0<=columna<7}
-//POST:{elimina el valor que se encuentra en la casilla determinada por fila y columna y deplaza los
-//elementos de la columna que se encuentran encima de ella hacia abajo}
-void vaciarCasillaTablero(Tablero &t, int fila, int columna);
-
-//PRE:{0<=columna<7, para que el módulo funcione, la columna no puede estar llena}
-//POST:{devuelve la fila de la primera casilla vacía de la columna dada}
-int filaPrimeraCasillaVacia(Tablero t,int columna);
-
-//PRE:{}
-//POST:{devuelve el número de filas que hay en el tablero}
-int numeroFilas(Tablero t);
-
+#endif
