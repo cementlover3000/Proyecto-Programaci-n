22e0eb3048b671aeca9cd92455d719e8b2ad985f
cementlover3000 <thewhenthe@gmail.com>
2025-01-16 16:01:51 +0100

    Update TAD_Tablero.h

--- src/TAD_Tablero.h
+++ src/TAD_Tablero.h
@@ -24,25 +24,31 @@ struct Tablero{
     int ocupadas;
 };
 
-//PRE:{n tiene que ser múltiplo de 2, 0<=fila<7, 0<=columna<7}
+//PRE:{n tiene que ser múltiplo de 2, 0<=fila<FILAS, 0<=columna<COLUMNAS}
 //POST:{inserta el valor n en la casilla del tablero indicada por fila y columna}
+//Complejidad: O(1)
 void insertarValorTablero(Tablero &t,int n, int fila,int columna);
 
 //PRE:{0<=fila<7, 0<=columna<7, la casilla debe de estar llena}
 //POST:{devuelve el valor de la casilla del tablero indicada por fila y columna}
+//Complejidad: O(1)
 int obtenerValorTablero(Tablero t,int fila,int columna);
 
 //PRE:{ 0<=columna<7}
 //POST:{devuelve true si la columna está llena y false en caso contrario}
+//Complejidad: O(n)
 bool columnaEstaLlena(Tablero t,int columna);
 
 //PRE:{}
 //POST:{devuelve true si el tablero está lleno y false en caso contrario}
+//Complejidad: O(n²)
 bool tableroEstaLleno(Tablero t);
 
 //PRE:{0<=columna<7}
 //POST:{elimina las casillas vacías intermedias y desplaza los elementos de una determinada casilla}
+//Complejidad: O(n)
 void eliminarCasillasVaciasIntermedias(Tablero &t, int columna);
+
 
 bool casillaEstaVacia(Tablero t, int fila, int columna);
 
