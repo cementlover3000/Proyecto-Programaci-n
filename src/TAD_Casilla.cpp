748386c7a6945ad235aa4afc882997193845c843
alebragon <alebragoncac200@gmail.com>
2025-01-16 16:50:50 +0100

    Update TAD_Casilla.cpp

--- src/TAD_Casilla.cpp
+++ src/TAD_Casilla.cpp
@@ -3,11 +3,11 @@
  * FUNDAMENTOS DE PROGRAMACIÓN
  *  Curso 2024/2025
 
-    Nombre: TAD_Casilla.cpp
-    Descripción: Implemetación del TAD Casilla para el proyecto Number Tiles
-    		       encargado de gestionar la información de una casilla del tablero.
-    		       
-    Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
+	Nombre: TAD_Casilla.cpp
+	Descripción: Implemetación del TAD Casilla para el proyecto Number Tiles
+   		    	encargado de gestionar la información de una casilla del tablero.
+
+	Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
 
  *
  */
@@ -18,31 +18,28 @@ using namespace std;
 
 
 void iniciar(Casilla &c){
-    c.ocupadas=0;
+	c.ocupadas=0;
 }
 
-bool estaVacia(Casilla c){
-    bool vacia;
-    if(c.ocupadas==0){
-   	 vacia=true;
-    }
-    else{
-   	 vacia=false;
-    }
-    return vacia;
+bool estaVacia(Casilla &c) {
+   return c.ocupadas == 0;
 }
 
 void vaciarCasilla(Casilla &c){
-    c.ocupadas=0;
+	c.ocupadas=0;
 }
 
-void insertarValorCasilla(Casilla &c, int n){
-    c.vector[0]=n;
-    c.ocupadas=c.ocupadas+1;
+void insertarValorCasilla(Casilla &c, int n) {
+	if (c.ocupadas < MAX) {
+    	c.vector[c.ocupadas] = n; // Almacena el valor en la posición ocupada
+    	c.ocupadas++;
+	}
 }
 
-int devolverValorCasilla(Casilla c){
-    int valor;
-    valor=c.vector[0];
-    return valor;
+int devolverValorCasilla(Casilla &c) {
+	if (c.ocupadas > 0) {
+    	return c.vector[0]; // Devuelve el valor almacenado
+	} else {
+    	return -1; // O algún valor que indique error
+	}
 }
