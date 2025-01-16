c3b0799343f851209268b6360c97f3f1a28d1fe7
cementlover3000 <thewhenthe@gmail.com>
2025-01-16 13:41:27 +0100

    Create PruebasTADTablero.h

---
+++ src/PruebasTADTablero.h
@@ -0,0 +1,28 @@
+/*
+ * PruebasTADTablero.h
+ *
+ *  Created on: 16 ene. 2025
+ *      Author: alumno
+ */
+
+#ifndef PRUEBASTADTABLERO_H_
+#define PRUEBASTADTABLERO_H_
+#include "TAD_Tablero.h"
+
+void inicializarTableroVacio(Tablero &t);
+
+void probarInsertarValor();
+
+void probarColumnaLlena();
+
+void probarTableroLleno();
+
+void probarEliminarCasillasVaciasIntermedias();
+
+void probarMaximoValor();
+
+void pruebasTADTablero();
+
+
+
+#endif /* PRUEBASTADTABLERO_H_ */
