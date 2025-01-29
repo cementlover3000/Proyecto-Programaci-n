62b292efe02fe85bbd42454e12c345646987de09
alebragon <alebragoncac200@gmail.com>
2025-01-29 17:59:23 +0100

    Update TAD_Casilla.h

--- src/TAD_Casilla.h
+++ src/TAD_Casilla.h
@@ -1,6 +1,5 @@
 #ifndef TAD_CASILLA_H_
 #define TAD_CASILLA_H_
-
 #include <iostream>
 using namespace std;
 
@@ -9,28 +8,20 @@ typedef int tVector[MAX];
 
 struct Casilla {
 	tVector vector;
-	int ocupadas; // Indica cuántos elementos están ocupando la casilla
+	int ocupadas;
 };
 
-// PRE: La casilla no ha sido inicializada
-// POST: Inicializa la casilla vacía
+
 void iniciar(Casilla &c);
 
-// PRE: La casilla ha sido inicializada
-// POST: Devuelve true si la casilla está vacía y false si está llena
-bool estaVacia(Casilla &c);
-
-// PRE: La casilla ha sido inicializada
-// POST: Borra la información almacenada en la casilla
-void vaciarCasilla(Casilla &c);
-
-// PRE: La casilla ha sido inicializada y no está llena
-// POST: Inserta el valor n en la casilla
 void insertarValorCasilla(Casilla &c, int n);
 
-// PRE: La casilla ha sido inicializada y no está vacía
-// POST: Devuelve el valor almacenado en la casilla
-int devolverValorCasilla(Casilla &c);
+bool estaVacia(Casilla c);
+
+int obtenerValorCasilla(Casilla c);
+
+void vaciarCasilla(Casilla &c);
 
 #endif
+
 
