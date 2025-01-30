17b031361d8cdc8d50be3779801941de4d8c1cd4
alebragon <alebragoncac200@gmail.com>
2025-01-30 13:18:34 +0100

    Update TAD_Casilla.cpp

--- src/TAD_Casilla.cpp
+++ src/TAD_Casilla.cpp
@@ -1,45 +1,32 @@
-/*
- *
- * FUNDAMENTOS DE PROGRAMACIÓN
- *  Curso 2024/2025
 
-	Nombre: TAD_Casilla.cpp
-	Descripción: Implemetación del TAD Casilla para el proyecto Number Tiles
-   		    	encargado de gestionar la información de una casilla del tablero.
-
-	Autor: Alejandro Bravo González y Alejandro Pulido Rodríguez
-
- *
- */
-
-#include <iostream>
+ #include <iostream>
 #include "TAD_Casilla.h"
 using namespace std;
 
-
-void iniciar(Casilla &c){
-	c.ocupadas=0;
-}
-
-bool estaVacia(Casilla &c) {
-   return c.ocupadas == 0;
-}
-
-void vaciarCasilla(Casilla &c){
-	c.ocupadas=0;
+void iniciar(Casilla &c) {
+	c.ocupadas = 0;
 }
 
 void insertarValorCasilla(Casilla &c, int n) {
 	if (c.ocupadas < MAX) {
-    	c.vector[c.ocupadas] = n; // Almacena el valor en la posición ocupada
+    	c.vector[0] = n;
     	c.ocupadas++;
 	}
 }
 
-int devolverValorCasilla(Casilla &c) {
-	if (c.ocupadas > 0) {
-    	return c.vector[0]; // Devuelve el valor almacenado
-	} else {
-    	return -1; // O algún valor que indique error
+bool estaVacia(Casilla c) {
+	return c.ocupadas == 0;
+}
+
+int obtenerValorCasilla(Casilla c) {
+	if (!estaVacia(c)) {
+    	return c.vector[0];
 	}
+	else {
+    	return -1;
+	}
+}
+
+void vaciarCasilla(Casilla &c) {
+	c.ocupadas = 0;
 }
