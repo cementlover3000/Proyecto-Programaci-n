147a30815b5d24a99027da0ac3e66a0035f5d021
alebragon <alebragoncac200@gmail.com>
2025-06-04 12:18:32 +0200

    Update TAD_Tablero.cpp

--- src/TAD_Tablero.cpp
+++ src/TAD_Tablero.cpp
@@ -5,267 +5,220 @@
 using namespace std;
 
 void vaciarTablero(Tablero& t){
-    for (int i = 0; i < FILAS; ++i){
-        for (int j = 0; j < COLUMNAS; ++j){
-            vaciarCasillaTablero(t, i, j);
-        }
-    }
-    t.ocupadas = 0;
+    int i;
+    int j;
+	for (i = 0; i < FILAS; ++i){
+    	for (j = 0; j < COLUMNAS; ++j){
+        	vaciarCasillaTablero(t, i, j);
+    	}
+	}
+	t.ocupadas = 0;
 }
 
-bool esPotenciaDe2(int num){
-    return (num > 0 && num < 4096 && (num & (num - 1)) == 0);
-}
-
-int redondearPotencia2(int num){
-    if (num <= 1){
-    	return 2;
-    }
-    if (num >= 4096){
-    	return 4096;
-    }
-
-    int potencia = 1;
-    while (potencia < num){
-        potencia *= 2;
-    }
-    return potencia;
-}
 
 void iniciarTableroAleatorio (Tablero &t, int filas, int columnas, int filasIniciales, int maximo) {
-	srand(time(nullptr));
-	for (int i = 0; i < filas; i++) {
-		for (int j = 0; j < columnas; j++) {
-			if (i < filasIniciales) {
-				insertarValorCasilla(t.tablero[i][j], pow(2,rand()%maximo + 1));
-			}
-			else {
-				vaciarCasilla(t.tablero[i][j]);
-			}
-		}
+    srand(time(nullptr));
+    int i;
+    int j;
+    for (i = 0; i < filasIniciales; i++) {
+   	 for (j = 0; j < columnas; j++) {
+   		 insertarValorTablero(t, i, j, pow(2,rand()%maximo + 1));
+   		 t.ocupadas++;
+   	 }
+    }
+}
+
+void iniciarTablero(Tablero& t) {
+	ifstream archivo("numberTiles.cnf");
+
+	int filas, columnas, comoIniciar, filasIniciales;
+	archivo >> filas >> columnas >> comoIniciar >> filasIniciales;
+
+	vaciarTablero(t);
+	t.ocupadas = 0;
+	t.filas = filas;
+	t.columnas = columnas;
+
+	if (comoIniciar == 0) {
+   	 int i;
+   	 int j;
+    	for (i = 0; i < filasIniciales; i++) {
+        	for (j = 0; j < columnas; j++) {
+            	int valor;
+            	archivo >> valor;
+            	insertarValorTablero(t, i, j, valor);
+            	t.ocupadas++;
+        	}
+    	}
 	}
-}
-
-bool iniciarTablero(Tablero& t) {
-    ifstream archivo("numberTiles.cnf");
-    if (!archivo) {
-        cout << "Error al abrir el archivo." << endl;
-        return false;
-    }
-
-    int filas, columnas, aleatorio, filasIniciales;
-    archivo >> filas >> columnas >> aleatorio >> filasIniciales;
-
-    if (filas < 3 || filas > FILAS || columnas < 5 || columnas > COLUMNAS ||
-        aleatorio < 0 || aleatorio > 12 || filasIniciales < 0 || filasIniciales > filas) {
-        cout << "Error en la configuración." << endl;
-        return false;
-    }
-
-    vaciarTablero(t);
-    t.ocupadas = 0;
-
-    if (aleatorio == 0) {
-        for (int i = 0; i < filasIniciales; ++i) {
-            for (int j = 0; j < columnas; ++j) {
-                int valor;
-                archivo >> valor;
-                if (!esPotenciaDe2(valor)) {
-                    valor = redondearPotencia2(valor);
-                    cout << "Aviso: Redondeado a " << valor << "." << endl;
-                }
-                insertarValorTablero(t, i, j, valor);
-                t.ocupadas++;
-            }
-        }
-    } else {
-        iniciarTableroAleatorio(t, filasIniciales, aleatorio);
-    }
-
-    return true;
-}
-
-void iniciarTableroAleatorio(Tablero &t,int filasIniciales, int maximo) {
-	srand(time(nullptr));
-	for (int i = 0; i < FILAS; i++){
-		for (int j = 0; j < COLUMNAS; j++){
-			if (i < filasIniciales){
-				insertarValorCasilla(t.tablero[i][j], pow(2,rand()%maximo + 1));
-			}
-			else {
-				iniciar(t.tablero[i][j]);
-			}
-		}
+	else {
+    	iniciarTableroAleatorio(t, filas, columnas, filasIniciales, comoIniciar);
 	}
 }
 
 
 void insertarValorTablero(Tablero &t, int fila, int columna, int valor){
-	if (casillaEstaVacia(t, fila, columna)) {
-    	insertarValorCasilla(t.tablero[fila][columna], valor);
-    	t.ocupadas = t.ocupadas+1;
-	}
+    vaciarCasillaTablero(t,fila,columna);
+	insertarValorCasilla(t.tablero[fila][columna], valor);
+	t.ocupadas = t.ocupadas+1;
 }
 
+
 bool casillaEstaVacia(Tablero t, int fila, int columna) {
-	return estaVacia(t.tablero[fila][columna]);
+    return estaVacia(t.tablero[fila][columna]);
 }
 
 int obtenerValorTablero(Tablero t, int fila, int columna) {
-	return obtenerValorCasilla(t.tablero[fila][columna]);
+    return obtenerValorCasilla(t.tablero[fila][columna]);
 }
 
 void vaciarCasillaTablero(Tablero &t, int fila, int columna) {
-	vaciarCasilla(t.tablero[fila][columna]);
-	t.ocupadas = t.ocupadas-1;
+    vaciarCasilla(t.tablero[fila][columna]);
+    t.ocupadas = t.ocupadas-1;
 }
 
 bool columnaEstaLlena(Tablero t, int columna) {
-	bool estaLlena;
-	int fila;
-	estaLlena = true;
-	for (fila = 0; fila < FILAS; fila++) {
-    	if (casillaEstaVacia(t, fila, columna)) {
-        	estaLlena = false;
-    	}
-	}
-	return estaLlena;
+    bool estaLlena;
+    int fila;
+    estaLlena = true;
+    for (fila = 0; fila < t.filas; fila++) {
+   	 if (casillaEstaVacia(t, fila, columna)) {
+   		 estaLlena = false;
+   	 }
+    }
+    return estaLlena;
 }
 
 int filaPrimeraCasillaVacia(Tablero t, int columna) {
-    int fila;
-    bool estaVacia;
-    estaVacia = false;
-	if (columnaEstaLlena(t, columna)) {
-   	 fila = -1;
-    }
-    else{
-  	  int i;
-  	  i = 0;
-  	  while(i < FILAS && !estaVacia){
-  	      if (casillaEstaVacia(t, i, columna)) {
-  	  		 fila=i;
-  	  		 estaVacia = true;
-  	      }
-  	      else{
-  			  i++;
-  	      }
-  	  }
-    }
-    return fila;
+	int fila;
+	bool estaVacia;
+	estaVacia = false;
+    if (columnaEstaLlena(t, columna)) {
+  	  fila = -1;
+	}
+	else{
+        int fila;
+        fila = 0;
+        while(fila < t.filas && !estaVacia){
+        	if (casillaEstaVacia(t, fila, columna)) {
+       		  estaVacia = true;
+        	}
+        	else{
+     		   fila++;
+        	}
+        }
+	}
+	return fila;
 }
 
 bool tableroEstaLleno(Tablero t) {
-	return t.ocupadas == FILAS * COLUMNAS;
+    return t.ocupadas == t.filas * t.columnas;
 }
 
 
-int numeroFilas(Tablero t) {
-	return FILAS;
-}
-
-int numeroColumnas(Tablero t) {
-	return COLUMNAS;
-}
 
 void fusionarCasillasAdyacentes(Tablero &t, int fila, int columna) {
-	int valorActual;
-	valorActual = obtenerValorTablero(t, fila, columna);
-	int sumaFusionada;
-	sumaFusionada = valorActual;
+    int valorActual;
+    valorActual = obtenerValorTablero(t, fila, columna);
+    int sumaFusionada;
+    sumaFusionada = valorActual;
 
-	int valorArriba;
-	valorArriba = obtenerValorTablero(t, fila - 1, columna);
-	if (valorArriba == valorActual) {
-    	sumaFusionada = sumaFusionada + valorArriba;
-    	vaciarCasillaTablero(t, fila - 1, columna);
-    	t.ocupadas=t.ocupadas-1;
+    int valorArriba;
+    valorArriba = obtenerValorTablero(t, fila - 1, columna);
+    if (valorArriba == valorActual) {
+   	 sumaFusionada = sumaFusionada + valorArriba;
+   	 vaciarCasillaTablero(t, fila - 1, columna);
+   	 t.ocupadas=t.ocupadas-1;
    }
 
    int valorAbajo;
    valorAbajo = obtenerValorTablero(t, fila + 1, columna);
    if (valorAbajo == valorActual) {
-    	sumaFusionada = sumaFusionada + valorAbajo;
-    	vaciarCasillaTablero(t, fila + 1, columna);
-    	t.ocupadas=t.ocupadas-1;
+   	 sumaFusionada = sumaFusionada + valorAbajo;
+   	 vaciarCasillaTablero(t, fila + 1, columna);
+   	 t.ocupadas=t.ocupadas-1;
    }
 
    int valorIzquierda;
    valorIzquierda = obtenerValorTablero(t, fila, columna - 1);
    if (valorIzquierda == valorActual) {
-    	sumaFusionada = sumaFusionada + valorIzquierda;
-    	vaciarCasillaTablero(t, fila, columna - 1);
-    	t.ocupadas=t.ocupadas-1;
+   	 sumaFusionada = sumaFusionada + valorIzquierda;
+   	 vaciarCasillaTablero(t, fila, columna - 1);
+   	 t.ocupadas=t.ocupadas-1;
    }
 
    int valorDerecha;
    valorDerecha = obtenerValorTablero(t, fila, columna + 1);
    if (valorDerecha == valorActual) {
-    	sumaFusionada = sumaFusionada + valorDerecha;
-    	vaciarCasillaTablero(t, fila, columna + 1);
-    	t.ocupadas=t.ocupadas-1;
+   	 sumaFusionada = sumaFusionada + valorDerecha;
+   	 vaciarCasillaTablero(t, fila, columna + 1);
+   	 t.ocupadas=t.ocupadas-1;
    }
 
    int nuevoValor;
    nuevoValor = 1;
    while (nuevoValor < sumaFusionada) {
-   	nuevoValor = nuevoValor * 2;
+  	 nuevoValor = nuevoValor * 2;
    }
    vaciarCasillaTablero(t, fila, columna);
    insertarValorTablero(t, fila, columna, nuevoValor);
 }
 
 void eliminarCasillasVaciasIntermedias(Tablero &t, int columna) {
-	int cuantos;
-	int fila;
-    cuantos = 0;
-	for (fila = 0; fila < FILAS; fila++) {
-    	if (estaVacia(t.tablero[fila][columna])) {
-        	cuantos = cuantos + 1;;
-    	}
-    	else {
-        	if (cuantos > 0) {
-            	t.tablero[fila - cuantos][columna] = t.tablero[fila][columna];
-            	vaciarCasillaTablero(t, fila, columna);
-        	}
-    	}
-	}
-	t.ocupadas = t.ocupadas - cuantos;
+    int cuantos;
+    int fila;
+	cuantos = 0;
+    for (fila = 0; fila < t.filas; fila++) {
+   	 if (casillaEstaVacia(t,fila,columna)) {
+   		 cuantos = cuantos + 1;;
+   	 }
+   	 else {
+   		 if (cuantos > 0) {
+   			 int valor;
+   			 valor = t.tablero[fila][columna];
+   			 vaciarCasillaTablero(t, fila - cuantos, columna);
+   		 	insertarValorTablero(t, fila - cuantos, columna, valor);
+       		 vaciarCasillaTablero(t, fila, columna);
+   		 }
+   	 }
+    }
 }
 
 
 int obtenerValorMaximo(Tablero t) {
-    int maxValor;
-    int valor;
-    int fila;
-    int columna;
-	maxValor = 0; //Porque el valor mínimo que puede haber en el tablero es 2
-	for (fila = 0; fila < FILAS; fila++) {
-    	for (columna = 0; columna < COLUMNAS; columna++) {
-        	if (!estaVacia(t.tablero[fila][columna])) {
-            	valor = obtenerValorCasilla(t.tablero[fila][columna]);
-            	if (valor > maxValor) {
-                	maxValor = valor;
-            	}
-        	}
-    	}
-	}
+	int maxValor;
+	int valor;
+	int fila;
+	int columna;
+    maxValor = 0; //Porque el valor mínimo que puede haber en el tablero es 2
+    for (fila = 0; fila < t.filas; fila++) {
+   	 for (columna = 0; columna < t.columnas; columna++) {
+   		 if (!casillaEstaVacia(t, fila, columna)) {
+       		 valor = obtenerValorTablero(t, fila, columna);
+       		 if (valor > maxValor) {
+           		 maxValor = valor;
+       		 }
+   		 }
+   	 }
+    }
 
-	return maxValor;
+    return maxValor;
 }
 
 
 int obtenerPuntuacionTablero (Tablero t){
-	int puntos = 0;
-		for (int i = 0; i < numeroFilas(t); i++) {
-			for (int j = 0; j < numeroColumnas(t); j++) {
-				if (!casillaEstaVacia(t, i, j))
-					puntos = puntos + obtenerValorTablero(t, i, j);
-			}
-		}
-	return puntos;
+    int puntos;
+    int i;
+    int j;
+    puntos = 0;
+   	 for (i = 0; i < t.filas; i++) {
+   		 for (j = 0; j < t.columnas; j++) {
+   			 if (!casillaEstaVacia(t, i, j))
+   				 puntos = puntos + obtenerValorTablero(t, i, j);
+   		 }
+   	 }
+    return puntos;
 }
-
 
 
 
