848513004feb3cd6b6ab3fb456b5b5a58073084c
alebragon <alebragoncac200@gmail.com>
2025-06-07 12:40:38 +0200

    Update PruebasTADTablero.cpp

--- src/PruebasTADTablero.cpp
+++ src/PruebasTADTablero.cpp
@@ -1,87 +1,549 @@
-void pruebaInsertarValorTablero() {
-    Tablero t;
-    insertarValorTablero(t, 0, 0, 2);
-    if (obtenerValorTablero(t, 0, 0) == 2) {
-        cout << "Prueba insertarValorTablero: Correcta" << endl;
-    } else {
-        cout << "Prueba insertarValorTablero: Incorrecta" << endl;
-    }
+#include <iostream>
+#include "PruebasTAD_Tablero.h"
+using namespace std;
+
+bool esPotenciaDeDos(int valor) {
+    int resultado;
+	if (valor <= 0) {
+    	resultado = false;
+	}
+	while (valor % 2 == 0) {
+    	valor = valor / 2;
+	}
+	resultado = valor == 1;
+	return resultado;
 }
+
+void pruebaVaciarTablero() {
+	Tablero t;
+	iniciarTablero(t);
+	vaciarTablero(t);
+	bool estaVacio;
+	int i;
+	int j;
+	estaVacio = true;
+	for (i = 0; i < t.filas; i++) {
+    	for (j = 0; j < t.columnas; j++) {
+        	if (!casillaEstaVacia(t, i, j)) {
+            	estaVacio = false;
+        	}
+    	}
+	}
+	if (estaVacio && t.ocupadas == 0) {
+    	cout << "Prueba Vaciar Tablero: OK" << endl;
+	}
+	else{
+   	 cout << "Prueba Vaciar Tablero: ERROR" << endl;
+	}
+}
+
+void pruebaIniciarTableroAleatorio() {
+	Tablero t;
+	vaciarTablero(t);
+	iniciarTableroAleatorio(t, FILAS, COLUMNAS, 3, 5); //
+
+	bool potenciaDeDos;
+	int i;
+	int j;
+	int valor;
+	potenciaDeDos = true;
+	for (i = 0; i < 3; i++) {
+    	for (j = 0; j < COLUMNAS; j++) {
+        	valor = obtenerValorTablero(t, i, j);
+        	if (!esPotenciaDeDos(valor)) {
+            	potenciaDeDos = false;
+        	}
+    	}
+	}
+	if (potenciaDeDos && t.ocupadas == 3 * COLUMNAS) {
+    	cout << "Prueba Iniciar Tablero Aleatorio: OK" << endl;
+	}
+	else {
+    	cout << "Prueba Iniciar Tablero Aleatorio: ERROR" << endl;
+	}
+}
+
+
+void pruebaInsertarValorTablero() {
+	//Prueba 1
+    Tablero t;
+    vaciarTablero(t);
+	iniciarTableroAleatorio(t, 5, 5, 3, 5);
+	insertarValorTablero(t, 0, 0, 5);
+
+	if(obtenerValorTablero(t, 0, 0) == 5 && t.ocupadas == 15){
+   	 cout << "Prueba 1 Insertar Valor Tablero: OK" << endl;
+	}
+	else{
+   	 cout << "Prueba 1 Insertar Valor Tablero: ERROR" << endl;
+	}
+
+	//Prueba 2
+	insertarValorTablero(t, 5, 5, 7);
+	if(obtenerValorTablero(t, 5, 5) == 7 && t.ocupadas == 16){
+    	cout << "Prueba 2 Insertar Valor Tablero: OK" << endl;
+	}
+	else{
+   	 cout << "Prueba 2 Insertar Valor Tablero: ERROR" << endl;
+	}
+
+	//Prueba 3
+    	insertarValorTablero(t, 4, 3, 22);
+    	if(obtenerValorTablero(t, 4, 3) == 22 && t.ocupadas == 17){
+        	cout << "Prueba 3 Insertar Valor Tablero: OK" << endl;
+    	}
+    	else{
+   		 cout << "Prueba 3 Insertar Valor Tablero: ERROR" << endl;
+    	}
+}
+
 
 void pruebaCasillaEstaVacia() {
-    Tablero t;
-    if (casillaEstaVacia(t, 0, 0)) {
-        cout << "Prueba casilla vacía inicial: Correcta" << endl;
-    } else {
-        cout << "Prueba casilla vacía inicial: Incorrecta" << endl;
-    }
-    insertarValorTablero(t, 0, 0, 4);
-    if (!casillaEstaVacia(t, 0, 0)) {
-        cout << "Prueba casilla ocupada: Correcta" << endl;
-    } else {
-        cout << "Prueba casilla ocupada: Incorrecta" << endl;
-    }
+	// Prueba 1
+	Tablero t;
+	vaciarTablero(t);
+	bool resultado;
+	resultado = casillaEstaVacia(t, 2, 1);
+
+	if (resultado) {
+    	cout << "Prueba 1 Casilla Esta Vacía: OK" << endl;
+	} else {
+    	cout << "Prueba 1 Casilla Esta Vacía: ERROR" << endl;
+	}
+
+	// Prueba 2
+	iniciarTableroAleatorio(t, 5, 5, 3, 5);
+	resultado = casillaEstaVacia(t, 0, 0);
+
+	if (!resultado) {
+    	cout << "Prueba 2 Casilla Esta Vacía: OK" << endl;
+	} else {
+    	cout << "Prueba 2 Casilla Esta Vacía: ERROR" << endl;
+	}
 }
+
 
 void pruebaObtenerValorTablero() {
+	Tablero t;
+	vaciarTablero(t);
+
+	// Prueba 1
+	int valor;
+	valor = obtenerValorTablero(t, 1, 1);
+	if (valor == 0) {
+    	cout << "Prueba 1 Obtener Valor Tablero: OK" << endl;
+	} else {
+    	cout << "Prueba 1 Obtener Valor Tablero: ERROR" << endl;
+	}
+
+	// Prueba 2:
+	insertarValorTablero(t, 0, 0, 16);
+	valor = obtenerValorTablero(t, 0, 0);
+	if (valor == 16) {
+    	cout << "Prueba 2 Obtener Valor Tablero: OK" << endl;
+	} else {
+    	cout << "Prueba 2 Obtener Valor Tablero: ERROR" << endl;
+	}
+
+	// Prueba 3
+	insertarValorTablero(t, 3, 2, 64);
+	valor = obtenerValorTablero(t, 3, 2);
+	if (valor == 64) {
+    	cout << "Prueba 3 Obtener Valor Tablero: OK" << endl;
+	} else {
+    	cout << "Prueba 3 Obtener Valor Tablero: ERROR" << endl;
+	}
+}
+
+
+void pruebaVaciarCasillaTablero(){
     Tablero t;
-    insertarValorTablero(t, 1, 1, 4);
-    if (obtenerValorTablero(t, 1, 1) == 4) {
-        cout << "Prueba obtenerValorTablero: Correcta" << endl;
-    } else {
-        cout << "Prueba obtenerValorTablero: Incorrecta" << endl;
+
+    // Prueba 1
+    vaciarTablero(t);
+    int ocupadas;
+    ocupadas = t.ocupadas;
+    vaciarCasillaTablero(t, 1, 1);
+    if (t.ocupadas == ocupadas && casillaEstaVacia(t, 1, 1)) {
+    	cout << "Prueba 1 Vaciar Casilla Tablero: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 1 Vaciar Casilla Tablero: ERROR" << endl;
+    }
+
+    // Prueba 2
+    vaciarTablero(t);
+    insertarValorTablero(t, 2, 2, 32);
+    ocupadas = t.ocupadas;
+    vaciarCasillaTablero(t, 2, 2);
+    if (t.ocupadas == ocupadas - 1 && casillaEstaVacia(t, 2, 2)) {
+    	cout << "Prueba 2 Vaciar Casilla Tablero: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 2 Vaciar Casilla Tablero: ERROR" << endl;
     }
 }
 
-void pruebaVaciarCasillaTablero() {
+void pruebaColumnaEstaLlena(){
     Tablero t;
-    insertarValorTablero(t, 0, 1, 8);
-    vaciarCasillaTablero(t, 0, 1);
-    if (casillaEstaVacia(t, 0, 1)) {
-        cout << "Prueba vaciarCasillaTablero: Correcta" << endl;
+    t.filas = FILAS;
+    t.columnas = COLUMNAS;
+    vaciarTablero(t);
+
+    // Prueba 1
+    bool resultado;
+    resultado = columnaEstaLlena(t, 1);
+    if (!resultado) {
+    	cout << "Prueba 1 Columna Esta Llena: OK" << endl;
     } else {
-        cout << "Prueba vaciarCasillaTablero: Incorrecta" << endl;
+    	cout << "Prueba 1 Columna Esta Llena: ERROR" << endl;
+    }
+
+    // Prueba 2
+    int fila;
+    for (fila = 0; fila < t.filas; fila++) {
+    	insertarValorTablero(t, fila, 0, 3);
+    }
+    resultado = columnaEstaLlena(t, 0);
+    if (resultado) {
+    	cout << "Prueba 2 Columna Esta Llena: OK" << endl;
+    } else {
+    	cout << "Prueba 2 Columna Esta Llena: ERROR" << endl;
+    }
+
+    // Prueba 3
+
+    for (fila = 0; fila < t.filas - 1; fila++) {
+    	insertarValorTablero(t, fila, 2, 3);
+    }
+    resultado = columnaEstaLlena(t, 2);
+    if (!resultado) {
+    	cout << "Prueba 3 Columna Esta Llena: OK" << endl;
+    } else {
+    	cout << "Prueba 3 Columna Esta Llena: ERROR" << endl;
+    }
+
+}
+
+
+void pruebaFilaPrimeraCasillaVacia(){
+    Tablero t;
+    t.filas = FILAS;
+    t.columnas = COLUMNAS;
+    vaciarTablero(t);
+
+    // Prueba 1
+    int fila;
+    int resultado;
+    for (fila = 0; fila < t.filas; fila++) {
+    	insertarValorTablero(t, fila, 0, 2);
+    }
+    resultado = filaPrimeraCasillaVacia(t, 0);
+    if (resultado == -1) {
+    	cout << "Prueba 1 Fila Primera Casilla Vacia: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 1 Fila Primera Casilla Vacia: ERROR" << endl;
+    }
+
+    // Prueba 2
+    vaciarTablero(t);
+    resultado = filaPrimeraCasillaVacia(t, 1);
+    if (resultado == 0) {
+    	cout << "Prueba 2 Fila Primera Casilla Vacia: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 2 Fila Primera Casilla Vacia: ERROR" << endl;
+    }
+
+    // Prueba 3:
+    insertarValorTablero(t, 0, 2, 4);
+    insertarValorTablero(t, 1, 2, 8);
+    resultado = filaPrimeraCasillaVacia(t, 2);
+    if (resultado == 2) {
+    	cout << "Prueba 3 Fila Primera Casilla Vacia: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 3 Fila Primera Casilla Vacia: ERROR" << endl;
     }
 }
 
-void pruebaColumnaEstaLlena() {
-    Tablero t;
-    for (int i = 0; i < FILAS; i++) {
-        insertarValorTablero(t, i, 0, 2);
-    }
-    if (columnaEstaLlena(t, 0)) {
-        cout << "Prueba columna llena: Correcta" << endl;
-    } else {
-        cout << "Prueba columna llena: Incorrecta" << endl;
-    }
-    vaciarCasillaTablero(t, 0, 0);
-    if (!columnaEstaLlena(t, 0)) {
-        cout << "Prueba columna no llena después de vaciar: Correcta" << endl;
-    } else {
-        cout << "Prueba columna no llena después de vaciar: Incorrecta" << endl;
-    }
-}
 
-void pruebaTableroEstaLleno() {
+void pruebaTableroEstaLleno(){
     Tablero t;
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
-            insertarValorTablero(t, i, j, 2);
-        }
+    t.filas = FILAS;
+    t.columnas = COLUMNAS;
+
+    // Prueba 1
+    vaciarTablero(t);
+    if (!tableroEstaLleno(t)) {
+    	cout << "Prueba 1 Tablero Esta Lleno: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 1 Tablero Esta Lleno: ERROR" << endl;
+    }
+
+    // Prueba 2
+    int fila;
+    int columna;
+    for (fila = 0; fila < t.filas; fila++) {
+    	for (columna = 0; columna < t.columnas; columna++) {
+        	insertarValorTablero(t, fila, columna, 2);
+    	}
     }
     if (tableroEstaLleno(t)) {
-        cout << "Prueba tablero lleno: Correcta" << endl;
-    } else {
-        cout << "Prueba tablero lleno: Incorrecta" << endl;
+    	cout << "Prueba 2 Tablero Esta Lleno: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 2 Tablero Esta Lleno: ERROR" << endl;
+    }
+
+    // Prueba 3
+    vaciarTablero(t);
+    for (fila = 0; fila < t.filas; fila++) {
+    	for (columna = 0; columna < t.columnas; columna++) {
+        	if (!(fila == 0 && columna == 0)) {
+            	insertarValorTablero(t, fila, columna, 4);
+        	}
+    	}
+    }
+    if (!tableroEstaLleno(t)) {
+    	cout << "Prueba 3 Tablero Esta Lleno: OK" << endl;
+    }
+    else {
+    	cout << "Prueba 3 Tablero Esta Lleno: ERROR" << endl;
     }
 }
 
-void ejecutarPruebasTAD_Tablero() {
-    pruebaIniciarTablero();
-    pruebaInsertarValorTablero();
-    pruebaCasillaEstaVacia();
-    pruebaObtenerValorTablero();
-    pruebaVaciarCasillaTablero();
-    pruebaColumnaEstaLlena();
-    pruebaTableroEstaLleno();
+void pruebaFusionarCasillasAdyacentes(){
+    	Tablero t;
+    	t.filas = FILAS;
+    	t.columnas = COLUMNAS;
+
+    	// Prueba 1
+    	vaciarTablero(t);
+    	insertarValorTablero(t, 1, 1, 2);
+    	insertarValorTablero(t, 1, 2, 2);
+    	fusionarCasillasAdyacentes(t, 1, 1);
+    	if (obtenerValorTablero(t, 1, 1) == 4 && casillaEstaVacia(t, 1, 2)) {
+        	cout << "Prueba 1 Fusionar Casillas Adyacentes: OK" << endl;
+    	}
+    	else {
+        	cout << "Prueba 1 Fusionar Casillas Adyacentes: ERROR" << endl;
+    	}
+
+    	//Prueba 2
+    	vaciarTablero(t);
+    	insertarValorTablero(t, 2, 1, 2);
+    	insertarValorTablero(t, 2, 2, 4);
+    	fusionarCasillasAdyacentes(t, 2, 1);
+    	if (obtenerValorTablero(t, 2, 1) == 2 && obtenerValorTablero(t, 2, 2) == 4) {
+        	cout << "Prueba 2 Fusionar Casillas Adyacentes: OK" << endl;
+    	}
+    	else {
+        	cout << "Prueba 2 Fusionar Casillas Adyacentes: ERROR" << endl;
+    	}
+
+    	// Prueba 3
+    	vaciarTablero(t);
+    	insertarValorTablero(t, 3, 1, 2);
+    	insertarValorTablero(t, 2, 1, 2);
+    	insertarValorTablero(t, 3, 2, 2);
+    	fusionarCasillasAdyacentes(t, 3, 1);
+    	if (obtenerValorTablero(t, 3, 1) == 8 && casillaEstaVacia(t, 2, 1) && casillaEstaVacia(t, 3, 2)) {
+        	cout << "Prueba 3 Fusionar Casillas Adyacentes: OK" << endl;
+    	}
+    	else {
+        	cout << "Prueba 3 Fusionar Casillas Adyacentes: ERROR" << endl;
+    	}
 }
+
+
+void pruebaEliminarCasillasVaciasIntermedias() {
+	Tablero t;
+	t.filas = FILAS;
+	t.columnas = COLUMNAS;
+
+	// Prueba 1
+	vaciarTablero(t);
+	insertarValorTablero(t, 0, 0, 2);
+	insertarValorTablero(t, 1, 0, 4);
+	insertarValorTablero(t, 3, 0, 8);
+	eliminarCasillasVaciasIntermedias(t, 0);
+	if (obtenerValorTablero(t, 1, 0) == 4 && obtenerValorTablero(t, 2, 0) == 8) {
+    	cout << "Prueba 1 Eliminar Casillas Vacias Intermedias: OK" << endl;
+	} else {
+    	cout << "Prueba 1 Eliminar Casillas Vacias Intermedias: ERROR" << endl;
+	}
+
+	//Prueba 2
+	vaciarTablero(t);
+	insertarValorTablero(t, 0, 0, 2);
+	insertarValorTablero(t, 1, 0, 4);
+	eliminarCasillasVaciasIntermedias(t, 0);
+	if (obtenerValorTablero(t, 0, 0) == 2 && obtenerValorTablero(t, 1, 0) == 4) {
+    	cout << "Prueba 2 Eliminar Casillas Vacias Intermedias: OK" << endl;
+	} else {
+    	cout << "Prueba 2 Eliminar Casillas Vacias Intermedias: ERROR" << endl;
+	}
+
+	// Prueba 3
+	vaciarTablero(t);
+	insertarValorTablero(t, 0, 0, 2);
+	insertarValorTablero(t, 1, 0, 4);
+	insertarValorTablero(t, 4, 0, 8);
+	insertarValorTablero(t, 5, 0, 16);
+	eliminarCasillasVaciasIntermedias(t, 0);
+	if (obtenerValorTablero(t, 1, 0) == 4 && obtenerValorTablero(t, 2, 0) == 8 && obtenerValorTablero(t, 3, 0) == 16) {
+    	cout << "Prueba 3 Eliminar Casillas Vacias Intermedias: OK" << endl;
+	} else {
+    	cout << "Prueba 3 Eliminar Casillas Vacias Intermedias: ERROR" << endl;
+	}
+}
+
+
+void pruebaObtenerValorMaximo() {
+	Tablero t;
+	t.filas = FILAS;
+	t.columnas = COLUMNAS;
+
+	// Prueba 1
+	vaciarTablero(t);
+	int maxValor;
+	maxValor = obtenerValorMaximo(t);
+	if (maxValor == 0) {
+    	cout << "Prueba 1 Obtener Valor Maximo: OK" << endl;
+	}
+	else {
+    	cout << "Prueba 1 Obtener Valor Maximo: ERROR" << endl;
+	}
+
+	// Prueba 2
+	vaciarTablero(t);
+	insertarValorTablero(t, 0, 0, 4);
+	insertarValorTablero(t, 1, 1, 4);
+	insertarValorTablero(t, 2, 2, 4);
+	maxValor = obtenerValorMaximo(t);
+	if (maxValor == 4) {
+    	cout << "Prueba 2 Obtener Valor Maximo: OK" << endl;
+	} else {
+    	cout << "Prueba 2 Obtener Valor Maximo: ERROR" << endl;
+	}
+
+	// Prueba 3
+	vaciarTablero(t);
+	int i;
+	int j;
+	for (i = 0; i < FILAS; i++) {
+    	for (j = 0; j < COLUMNAS; j++) {
+        	insertarValorTablero(t, i, j, (i + 1) * (j + 1));
+    	}
+	}
+	maxValor = obtenerValorMaximo(t);
+	if (maxValor == 49) {
+    	cout << "Prueba 3 Obtener Valor Maximo: OK" << endl;
+	}
+	else {
+    	cout << "Prueba 3 Obtener Valor Maximo: ERROR" << endl;
+	}
+}
+
+
+void pruebaObtenerPuntuacionTablero() {
+	Tablero t;
+	t.filas = FILAS;
+	t.columnas = COLUMNAS;
+
+	// Prueba 1
+	vaciarTablero(t);
+	int puntuacion;
+	puntuacion = obtenerPuntuacionTablero(t);
+	if (puntuacion == 0) {
+    	cout << "Prueba 1 Obtener Puntuacion Tablero: OK" << endl;
+	}
+	else {
+    	cout << "Prueba 1 Obtener Puntuacion Tablero: ERROR" << endl;
+	}
+	// Prueba 2
+	vaciarTablero(t);
+	insertarValorTablero(t, 0, 0, 2);
+	insertarValorTablero(t, 1, 1, 8);
+	insertarValorTablero(t, 2, 2, 4);
+	puntuacion = obtenerPuntuacionTablero(t);
+	if (puntuacion == (2 + 8 + 4)) {
+    	cout << "Prueba 2 Obtener Puntuacion Tablero: OK" << endl;
+	}
+	else {
+    	cout << "Prueba 2 Obtener Puntuacion Tablero: ERROR" << endl;
+	}
+	// Prueba 5
+	vaciarTablero(t);
+	int i;
+	int j;
+	int suma;
+	int valor;
+	suma = 0;
+	for (i = 0; i < FILAS; i++) {
+    	for (j = 0; j < COLUMNAS; j++) {
+        	valor = (i + 1) * (j + 1);
+        	insertarValorTablero(t, i, j, valor);
+        	suma = suma + valor;
+    	}
+	}
+	puntuacion = obtenerPuntuacionTablero(t);
+	if (puntuacion == suma) {
+    	cout << "Prueba 3 Obtener Puntuacion Tablero: OK" << endl;
+	}
+	else {
+    	cout << "Prueba 3 Obtener Puntuacion Tablero: ERROR" << endl;
+	}
+}
+
+void pruebaNumeroFilas() {
+	Tablero t;
+	t.filas = FILAS;
+	if (numeroFilas(t) == FILAS) {
+    	cout << "Prueba Numero Filas: OK" << endl;
+	}
+	else {
+    	cout << "Prueba Numero Filas: ERROR" << endl;
+	}
+}
+
+void pruebaNumeroColumnas() {
+	Tablero t;
+	t.columnas = COLUMNAS;
+	if (numeroColumnas(t) == COLUMNAS) {
+    	cout << "Prueba Numero Columnas: OK" << endl;
+	}
+	else {
+    	cout << "Prueba Numero Columnas: ERROR" << endl;
+	}
+}
+
+
+void ejecutarPruebasTAD_Tablero() {
+	cout << "Inicio Pruebas TAD_Tablero:" << endl;
+
+	pruebaVaciarTablero();
+	pruebaIniciarTableroAleatorio();
+	//pruebaIniciarTablero();
+	pruebaInsertarValorTablero();
+	pruebaCasillaEstaVacia();
+	pruebaObtenerValorTablero();
+	pruebaVaciarCasillaTablero();
+	pruebaColumnaEstaLlena();
+	pruebaFilaPrimeraCasillaVacia();
+	pruebaTableroEstaLleno();
+	pruebaFusionarCasillasAdyacentes();
+	pruebaEliminarCasillasVaciasIntermedias();
+	pruebaObtenerValorMaximo();
+	pruebaObtenerPuntuacionTablero();
+	pruebaNumeroFilas();
+	pruebaNumeroColumnas();
+
+	cout << "Fin Pruebas TAD_Tablero" << endl;
+}
+
+
+
